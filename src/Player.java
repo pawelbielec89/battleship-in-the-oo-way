4d32f0f10c4854bd78dd70c2ad8518ebd6e98fe2
Jakub Wojtasik <jakubwojtasikzsp@gmail.com>
2018-03-27 17:44:09 +0200

    Implement tryShoot()

--- src/Player.java
+++ src/Player.java
@@ -1,14 +1,17 @@
 public class Player {
   String name;
   int score;
+  Inputs input = new Inputs();
 
   public Player(String name) {
     this.name = name;
     this.score = 117;
   }
 
-  public void tryShoot(Board board, int x, int y) {
-    Square sqr = board.getSquare(x, y);
+  public void tryShoot(Board board) {
+    int cordX = input.getCord('x');
+    int cordY = input.getCord('y');
+    Square sqr = board.getSquare(cordX, cordY);
 
     if (sqr.getCanShoot()) {
       sqr.setCanShoot(false);
