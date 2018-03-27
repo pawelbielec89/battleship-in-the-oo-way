d1e4733afdee6a3b9daf10f8060a171e2712bd5f
Jakub Wojtasik <jakubwojtasikzsp@gmail.com>
2018-03-27 11:18:14 +0200

    Square class create

--- src/Square.java
+++ src/Square.java
@@ -1,1 +1,21 @@
-public class Square {}
+public class Square {
+  public boolean isOccupiedArea = false;
+  public boolean canShoot = true;
+  public boolean isShip = false;
+
+  public Square() {}
+
+  public void changeIsShipToTrue() {
+    isShip = true;
+  }
+
+  public void changeCanShootToFalse() {
+    canShoot = false;
+  }
+
+  public void changeIsOccupiedAreaToTrue() {
+    isOccupiedArea = true;
+  }
+
+  public void getFields() {}
+}
