8d9b4816dfb49979c7f8e9b77aec16a2108cf87d
Jakub Wojtasik <jakubwojtasikzsp@gmail.com>
2018-03-28 13:19:21 +0200

    Add getter functions

--- src/Ship.java
+++ src/Ship.java
@@ -1,20 +1,38 @@
 public class Ship {
-    private int x;
-    private int y;
-    private boolean isVertical;
-    private int length;
-    private boolean isAlive = true;
+  private int x;
+  private int y;
+  private boolean isVertical;
+  private int length;
+  private boolean isAlive = true;
 
+  public Ship(int x, int y, boolean isVertical, int length) {
+    this.x = x;
+    this.y = y;
+    this.isVertical = isVertical;
+    this.length = length;
+  }
 
-    public Ship(int x, int y, boolean isVertical, int length){
-        this.x = x;
-        this.y = y;
-        this.isVertical = isVertical;
-        this.length = length;
-    }
+  public void setAlive(boolean alive) {
+    this.isAlive = alive;
+  }
 
-    public setAlive(boolean alive){
-        ship.isAlive = alive;
-    }
+  public int getXCord() {
+    return x;
+  }
 
+  public int getYCord() {
+    return y;
+  }
+
+  public boolean getIsVertical() {
+    return isVertical;
+  }
+
+  public int getLength() {
+    return lenght;
+  }
+
+  public boolean getIsAlive() {
+    return isAlive;
+  }
 }
