96ab6d9f48e3f80f4577100244e9a3dcad175ba6
Paul <bielcu@gmail.com>
2018-04-07 18:44:31 +0200

    fix push error

--- src/AI.java
+++ src/AI.java
@@ -1,42 +1,26 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-public class AI extends Player {
+public abstract class AI extends Player {
+  List<Integer> allBoardCoords = new ArrayList<>();
+
   public AI() {
-    List<String> allBoardCoords = new ArrayList<>();
-    for (i = 0; i < 100; i++) {
-      allBoardCoords.add(i);
+    for (int i = 0; i < 100; i++) {
+      this.allBoardCoords.add(i);
     }
   }
 
-  public void easyAiShot(Board board) {
-    int randomCoord = getRandomCoordsFromList(allBoardCoords);
-    String stringCoords = Integer.toString(randomCoord);
-    if (stringCoords.length() < 2) {
-      int xCoord = 0;
-      int yCoord = randomCoord;
-    } else {
+  public int getRandomCoordFromList(List<Integer> array) {
+    int randomCoordFromList = new Random().nextInt(array.size());
+    return randomCoordFromList;
+  }
 
-      char xCoordChar = text.charAt(0);
-      char yCoordChar = text.charAt(1);
-      int xCoord = Character.getNumericValue(xCoordChar);
-      int xCoord = Character.getNumericValue(yCoordChar);
-      System.out.println(xCoord);
-    }
-    System.out.println(xCoord);
-    System.out.println(yCoord);
-    Square squareToShot = getSquare(xCoord, yCoord);
-    suqareToShot.setCanShoot(False);
-    for (int i = 0; i < allBoardCoords.length; i++) {
-      if (randomCoord == allBoardCoords[i]) {
-        allBoardCoords.remove(allBoardCoords[i]);
+  public void removeCoordsFromList(int randomCoord) {
+    for (int i = 0; i < allBoardCoords.size(); i++) {
+      if (randomCoord == allBoardCoords.get(i)) {
+        allBoardCoords.remove(allBoardCoords.get(i));
       }
     }
-  }
-
-  private int getRandomCoordsFromList(ArrayList allBoardCoords) {
-    Random rand = new Random();
-    int randomCoord = allBoardCoords.get(rand.nextInt(allBoardCoords.size()));
-    return randomCoord;
   }
 }
