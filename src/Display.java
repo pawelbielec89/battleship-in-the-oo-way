23ab8d73564776e5e61d0b01d6112566fe8addb1
SStreich <selene.streich@gmail.com>
2018-04-10 18:49:03 +0200

    wrongCoordMassage() methods added, chooseShip, chooseIsVertical and chooseCoords methods modified

--- src/Display.java
+++ src/Display.java
@@ -5,19 +5,20 @@ import java.util.List;
 public class Display {
 
 
+  private List<String> usedShips = new ArrayList<>();  
+
+
   public void print(String text) {
     System.out.println(text);
   }
-
-
+  
   public String chooseShip() {
-    Inputs input = new Inputs();    
-    List<String> ships = new ArrayList<>(Arrays.asList("Destroyer", "Submarine", "Cruiser", "Battleship", "Cruiser"));    
-    List<Integer> usedShips = new ArrayList<>();
-    int index = 6;
+    List<String> ships = new ArrayList<>(Arrays.asList("Destroyer", "Submarine", "Cruiser", "Battleship", "Carrier"));    
+    int index;
+    boolean shipChosen = false;
     String shipName = "";
 
-    while (index < 0 || index > 5) {
+    while (shipChosen == false) {
       print("Ships which you can choose: \n"
             + "1. Destroyer - 2 spaces long\n"
             + "2. Submarine - 3 spaces long\n"
@@ -25,74 +26,88 @@ public class Display {
             + "4. Battleship - 4 spaces long\n"
             + "5. Carrier - 5 spaces long\n"
             + "Choose number of ship you want to put on now");
-      index = input.getInt();
-      usedShips.add(index);
+      Inputs input = new Inputs();          
+      index = input.getInt() - 1;
 
-      if (index < 0 || index > 5) { print("There is no such an option!"); }  
-      else if (usedShips.contains(index)) { print("You've already placed this ship!"); }
-      else { index--; shipName = ships.get(index); }    
+      if (index < 0 || index > 4) { print("There is no such an option!"); }  
+      else { shipName = ships.get(index);
+        if (usedShips.contains(shipName)) { print("You've already placed this ship!\n"); }
+        else { usedShips.add(shipName); shipChosen = true; }  
+      }
     }
-    
     return shipName;
-    
   }
 
   public boolean chooseIsVertical(){
-    Inputs input = new Inputs();    
     int position = 0;
     boolean isVertical = false; 
     while (position != 1 && position != 2) {
+    Inputs input = new Inputs();          
     print("Do you want to place this ship vertical or horizontal?\n"
     + "1. Vertical\n" + "2. Horizontal");
     position = input.getInt();
     if (position == 1) { isVertical = true; }
-    else if (position == 2) { isVertical = false; }
-    else { print("There is no such an option!"); }
+    else if (position != 2) { print("There is no such an option!"); }
     }
     return isVertical;
   }
 
 
-  public int[] chooseCords() {
-    Inputs input = new Inputs();    
-    int[] cords = new int[2];
-    print("Enter coordinates: ");
-    String cord = input.getInput();
-    char[] cordsAsChars = cord.toCharArray();      
-    cords[0] = (int)cordsAsChars[0] - 97;
-    cords[1] = (int)cordsAsChars[1] - 49;
+  public char[] chooseCoords() {
+      print("Enter coordinates: ");
+      Inputs input = new Inputs();       
+      String coord = input.getInput();
+      char[] coordsAsChars = coord.toCharArray();
+      coordsAsChars[0] = Character.toLowerCase(coordsAsChars[0]);
 
-    return cords;
+      
+      return coordsAsChars;
+   }
+  public void wrongCoordsMassage1(){     
+    print("Wrong coordinates!\n" 
+          +"You should enter a letter between a-j and a number between 1-9, like a4 :)\n");
+   }
+
+  public void wrongCoordsMassage2(){ 
+    print("Wrong coordinates!\n" 
+            + "Remember that you can't put your ship on area occupied by another one."); 
+   }
+
+  public void wrongCoordsMassage3() {
+     print("You've already shooted in this place!");
+   }
+    
+  public void wrongCoordsMassage4(String shipName) {
+    usedShips.remove(shipName);    
+    print("You can't set ship on occupied area! Try again.");
   }
 
-
-  public char generateChar(Board board, int x, int y) {
+  public char assignCharInPosition(Board board, int x, int y) {
     Square sqr = board.getSquare(x, y);
     // roboczo nazwane, nie wiem jak się dostać do tej funkcji w konkretnym obiekcie
     if (sqr.getCanShoot()) {
-      if (sqr.getIsShip() && board.getIsHidden() == false) {
-        return 'X';
-      } else {
-        return '~';
-      }
-    } else {
-      return 'O';
-    }
+      if (sqr.getIsShip() && board.getIsHidden() == false) { return 'X';
+      } else if (sqr.getIsOccupiedArea() == true) { return '#'; }
+      else { return '~'; }
+    } 
+    else { return 'O';}
   }
 
   char[][] charBoard = new char[10][10];
 
-  //   public
 
-  public void displayBoard(Board board1, Board board2) {
+  public void displayBoards(Board board1, Board board2) {
     String letters = "   A  B  C  D  E  F  G  H  I  J ";
     String[] numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
     char sign;
-
+    
+    
+    print("            P L A Y E R 1                  P L A Y E R 2 ");
+    print(letters + letters);
+    
     for (int i = 0; i < 10; i++) {
       if (i == 0) {
-        print("            P L A Y E R 1                  P L A Y E R 2 ");
-        print(letters + letters);
+
       }
       if (i != 9) {
         System.out.print(numbers[i] + " ");
@@ -110,9 +125,9 @@ public class Display {
         }
 
         if (j < 10) {
-          sign = generateChar(board1, i, j);
+          sign = assignCharInPosition(board1, i, j);
         } else {
-          sign = generateChar(board2, i, j - 10);
+          sign = assignCharInPosition(board2, i, j - 10);
         }
 
         System.out.print(" " + sign + " ");
@@ -121,8 +136,7 @@ public class Display {
       print("");
     }
   }
-  // char[][] charBoard = new char[10][10];
 
-  //   public
+//  public void displaySingleBoard(Board board) {}
 
 }
