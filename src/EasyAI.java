8894eefdaf703b411d6fc609d2a37500b7e3c451
Paul <bielcu@gmail.com>
2018-04-10 11:42:25 +0200

    Segregate methods in Player abstract class and its extending classes, implement proper MedAI and EasyAI classes

--- src/EasyAI.java
+++ src/EasyAI.java
@@ -1,30 +1,13 @@
 public class EasyAI extends AI {
 
-  public EasyAI(String name) {
-    this.name = name;
-    this.score = 117;
-  }
-
-  public int[] easyAiShot() {
-    int[] shotCoords = {0, 0};
-    int randomCoord = getRandomCoordFromList(allBoardCoords);
-    //  if allBoardCoords.isEmpty()
-    removeCoordsFromList(randomCoord);
-    String stringCoords = Integer.toString(randomCoord);
-    int xCoord;
-    int yCoord;
-    if (stringCoords.length() < 2) {
-      xCoord = 0;
-      yCoord = randomCoord;
-    } else {
-      char xCoordChar = stringCoords.charAt(0);
-      char yCoordChar = stringCoords.charAt(1);
-      xCoord = Character.getNumericValue(xCoordChar);
-      yCoord = Character.getNumericValue(yCoordChar);
-    }
-
-    shotCoords[0] = xCoord;
-    shotCoords[1] = yCoord;
-    return shotCoords;
+  /*
+  Get unique random coordinates from list, remove them from it and set canShoot
+  of square with given coords to false
+   * @param Board - board of AI enemy
+   * @return none
+  */
+  public void easyAIShot(Board board) {
+    shotCoords = randomAiCoordsToShot();
+    tryShoot(board, shotCoords[0], shotCoords[1]);
   }
 }
