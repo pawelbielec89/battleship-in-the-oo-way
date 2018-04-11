1db2651599dca2c4ba05c63b8bdd1dac9988414d
Jakub Wojtasik <jakubwojtasikzsp@gmail.com>
2018-04-11 10:26:45 +0200

    Clean code

--- src/Display.java
+++ src/Display.java
@@ -4,81 +4,94 @@ import java.util.List;
 
 public class Display {
 
-
-  private List<String> usedShips = new ArrayList<>();  
-
+  private List<String> usedShips = new ArrayList<>();
 
   public void print(String text) {
     System.out.println(text);
   }
-  
+
   public String chooseShip() {
-    List<String> ships = new ArrayList<>(Arrays.asList("Destroyer", "Submarine", "Cruiser", "Battleship", "Carrier"));    
+    List<String> ships =
+        new ArrayList<>(
+            Arrays.asList("Destroyer", "Submarine", "Cruiser", "Battleship", "Carrier"));
     int index;
     boolean shipChosen = false;
     String shipName = "";
 
     while (shipChosen == false) {
-      print("Ships which you can choose: \n"
-            + "1. Destroyer - 2 spaces long\n"
-            + "2. Submarine - 3 spaces long\n"
-            + "3. Cruiser - 3 spaces long\n"
-            + "4. Battleship - 4 spaces long\n"
-            + "5. Carrier - 5 spaces long\n"
-            + "Choose number of ship you want to put on now");
-      Inputs input = new Inputs();          
+      print(
+          "Ships which you can choose: \n"
+              + "1. Destroyer - 2 spaces long\n"
+              + "2. Submarine - 3 spaces long\n"
+              + "3. Cruiser - 3 spaces long\n"
+              + "4. Battleship - 4 spaces long\n"
+              + "5. Carrier - 5 spaces long\n"
+              + "Choose number of ship you want to put on now");
+      Inputs input = new Inputs();
       index = input.getInt() - 1;
 
-      if (index < 0 || index > 4) { print("There is no such an option!"); }  
-      else { shipName = ships.get(index);
-        if (usedShips.contains(shipName)) { print("You've already placed this ship!\n"); }
-        else { usedShips.add(shipName); shipChosen = true; }  
+      if (index < 0 || index > 4) {
+        print("There is no such an option!");
+      } else {
+        shipName = ships.get(index);
+        if (usedShips.contains(shipName)) {
+          print("You've already placed this ship!\n");
+        } else {
+          usedShips.add(shipName);
+          shipChosen = true;
+        }
       }
     }
     return shipName;
   }
 
-  public boolean chooseIsVertical(){
+  public boolean chooseIsVertical() {
     int position = 0;
-    boolean isVertical = false; 
+    boolean isVertical = false;
     while (position != 1 && position != 2) {
-    Inputs input = new Inputs();          
-    print("Do you want to place this ship vertical or horizontal?\n"
-    + "1. Vertical\n" + "2. Horizontal");
-    position = input.getInt();
-    if (position == 1) { isVertical = true; }
-    else if (position != 2) { print("There is no such an option!"); }
+      Inputs input = new Inputs();
+      print(
+          "Do you want to place this ship vertical or horizontal?\n"
+              + "1. Vertical\n"
+              + "2. Horizontal");
+      position = input.getInt();
+      if (position == 1) {
+        isVertical = true;
+      } else if (position != 2) {
+        print("There is no such an option!");
+      }
     }
     return isVertical;
   }
 
-
   public char[] chooseCoords() {
-      print("Enter coordinates: ");
-      Inputs input = new Inputs();       
-      String coord = input.getInput();
-      char[] coordsAsChars = coord.toCharArray();
-      coordsAsChars[0] = Character.toLowerCase(coordsAsChars[0]);
+    print("Enter coordinates: ");
+    Inputs input = new Inputs();
+    String coord = input.getInput();
+    char[] coordsAsChars = coord.toCharArray();
+    coordsAsChars[0] = Character.toLowerCase(coordsAsChars[0]);
 
-      
-      return coordsAsChars;
-   }
-  public void wrongCoordsMassage1(){     
-    print("Wrong coordinates!\n" 
-          +"You should enter a letter between a-j and a number between 1-9, like a4 :)\n");
-   }
+    return coordsAsChars;
+  }
 
-  public void wrongCoordsMassage2(){ 
-    print("Wrong coordinates!\n" 
-            + "Remember that you can't put your ship on area occupied by another one."); 
-   }
+  public void wrongCoordsMassage1() {
+    print(
+        "Wrong coordinates!\n"
+            + "You should enter a letter between a-j and a number between 1-9, like a4 :)\n");
+  }
+
+  public void wrongCoordsMassage2() {
+    print(
+        "Wrong coordinates!\n"
+            + "Remember that you can't put your ship on area occupied by another one.");
+  }
 
   public void wrongCoordsMassage3() {
-     print("You've already shooted in this place!");
-   }
-    
+    print("You've already shooted in this place!");
+  }
+
   public void wrongCoordsMassage4(String shipName) {
-    usedShips.remove(shipName);    
+    usedShips.remove(shipName);
     print("You can't set ship on occupied area! Try again.");
   }
 
@@ -86,29 +99,31 @@ public class Display {
     Square sqr = board.getSquare(x, y);
     // roboczo nazwane, nie wiem jak się dostać do tej funkcji w konkretnym obiekcie
     if (sqr.getCanShoot()) {
-      if (sqr.getIsShip() && board.getIsHidden() == false) { return 'X';
-      } else if (sqr.getIsOccupiedArea() == true) { return '#'; }
-      else { return '~'; }
-    } 
-    else { return 'O';}
+      if (sqr.getIsShip() && board.getIsHidden() == false) {
+        return 'X';
+      } else if (sqr.getIsOccupiedArea() == true) {
+        return '#';
+      } else {
+        return '~';
+      }
+    } else {
+      return 'O';
+    }
   }
 
   char[][] charBoard = new char[10][10];
-
 
   public void displayBoards(Board board1, Board board2) {
     String letters = "   A  B  C  D  E  F  G  H  I  J ";
     String[] numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
     char sign;
-    
-    
+
     print("            P L A Y E R 1                  P L A Y E R 2 ");
     print(letters + letters);
-    
-    for (int i = 0; i < 10; i++) {
-      if (i == 0) {
 
-      }
+    for (int i = 0; i < 10; i++) {
+      if (i == 0) {}
+
       if (i != 9) {
         System.out.print(numbers[i] + " ");
       } else {
@@ -137,6 +152,6 @@ public class Display {
     }
   }
 
-//  public void displaySingleBoard(Board board) {}
+  //  public void displaySingleBoard(Board board) {}
 
 }
--- src/Game.java
+++ src/Game.java
@@ -1,13 +1,16 @@
+import java.util.ArrayList;
+
 public class Game {
+  static Player player1 = new Player();
+  static Player player2 = new Player();
+  static Board board1 = new Board();
+  static Board board2 = new Board();
+  static Display disp = new Display();
+  static boolean is_game = true;
+  static ArrayList<Player> players = new ArrayList<Player>();
+  static ArrayList<Board> boards = new ArrayList<Board>();
+
   public Game() {
-    Player player1 = new Player();
-    Player player2 = new Player();
-    Board board1 = new Board();
-    Board board2 = new Board();
-    Display disp = new Display();
-    boolean is_game = true;
-    ArrayList<Player> players = new ArrayList<Player>();
-    ArrayList<Board> boards = new ArrayList<Board>();
     players.add(player1);
     players.add(player2);
     boards.add(board1);
@@ -29,28 +32,21 @@ public class Game {
       Player opposite_player = players.get(opposite_player_index);
       Board current_board = boards.get(current_player_index);
       Board opposite_board = boards.get(opposite_player_index);
-      current_player(tryShoot(opposite_board, disp));
+      System.out.println("Player " + current_player_index);
+      current_player.tryShoot(opposite_board, disp);
       is_game = checkGameStatus();
       if (!is_game) break;
     }
   }
 
-  public void prepareGame(boolean is_game) {
+  public void prepareGame() {
     disp.print("1player");
-    Ship[] p1Ships = createShips();
+    Ship[] p1Ships = new Ship().createShips(board1);
     disp.print("2player");
-
-    Ship[] p2Ships = createShips();
-    for (int i = 0; i < 5; i++) {
-      Ship.setShipOnSquares(p1Ships[i], board1);
-      Ship.setShipOnSquares(p2Ships[i], board2);
-    }
+    Ship[] p2Ships = new Ship().createShips(board2);
   }
 
   public boolean checkGameStatus() {
-    if (players.get(0).health_points == 0 || players.get(1).health_points == 0) {
-      return false;
-    }
     return true;
   }
 }
